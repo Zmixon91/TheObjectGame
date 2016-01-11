475b875e39e57181dbde8431bf61e1259bb8224d
Zmixon91 <zmixon91@gmail.com>
2016-01-11 00:51:52 -0700

    changed health to stickHealth in preperation for stick to fight back

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
-var health = 100;
+var stickHealth = 100;
+var playerHealth = 100;
 var win = false;
 var overKill = 0;
 var hElem = document.getElementById('h-elem');
@@ -6,33 +7,33 @@ var bElem = document.getElementById('b-elem');
 var aElem = document.getElementById('attack-img');
 
 var attacks = {
-	kick: 10,
-	punch: 5,
-	slap: 1
+    kick: 10,
+    punch: 5,
+    slap: 1
 };
 
-function attack(attackType){
-	aElem.className = "attack-img";
-        health -= attacks[attackType];
-    if (health <= 0 && !win) {
-        health = 0;
+function attack(attackType) {
+    aElem.className = "attack-img";
+    stickHealth -= attacks[attackType];
+    if (stickHealth <= 0 && !win) {
+        stickHealth = 0;
         win = true;
     } else if (win) {
-        health = 0;
+        stickHealth = 0;
         overKill += 1;
     }
-	update();
+    update();
 }
 
-function update(){
-	hElem.innerText = String(health);
-	setTimeout(function() {
-		aElem.className="hidden";
-	}, 500);
+function update() {
+    hElem.innerText = String(stickHealth);
+    setTimeout(function () {
+        aElem.className = "hidden";
+    }, 500);
     if (win) {
         bElem.innerText = String("YOU WIN");
         if (overKill) {
-            if (overKill <= 4) {}
+            if (overKill <= 4) { }
             else if (overKill <= 10) {
                 bElem.innerText = String("Stop! You won!");
             } else if (overKill <= 12) {
@@ -40,7 +41,7 @@ function update(){
             } else if (overKill <= 15) {
                 bElem.innerText = String("You're a monster!");
             } else if (overKill >= 16) {
-                window.location.href='http://www.angermgmt.com/'
+                window.location.href = 'http://www.angermgmt.com/'
             }
         }
     }
