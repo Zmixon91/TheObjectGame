81771983ecf6868c0f2405b85ea408b6f831d0ac
Zmixon91 <zmixon91@gmail.com>
2016-01-17 02:38:10 -0700

    re-re-wrote items list, actually fixed this time

--- items.js
+++ items.js
@@ -1,5 +1,6 @@
 // Item Constructor
 //array.splice method
+var id = 0;
 var Item = function (name, modHealth, modAttack, affectsPlayer, desc) {
     // Define items
     this.name = name;
@@ -7,6 +8,7 @@ var Item = function (name, modHealth, modAttack, affectsPlayer, desc) {
     this.modAttack = modAttack;
     this.affectsPlayer = affectsPlayer;
     this.desc = desc;
+    this.id = id++;
     // Constructed functions
     this.add = function () {
         player.inventory.push(this);
@@ -20,28 +22,32 @@ var Item = function (name, modHealth, modAttack, affectsPlayer, desc) {
     }
     this.draw = function () {
         // Draws itself to the page
-        $("#whatever").append("<li>" + this.name + " " + this.desc + "</li>");
+        $("#whatever").append("<li class=" + this.id + " onclick=''>" + this.name + " " + this.desc + "</li>");
     }
 }
-var items = [
+var items = {
     // List of items available in the game
-    // LIST DUMMY, why didn't you have this in an array before?
-    { potion: new Item("Health Potion", 10, 0, true, "Tastes like cherries!") },
-    { sword: new Item("Longsword", 0, .5, true, "A shiny longsword with a sharp edge.") },
-    { poison: new Item("Alien Ant Poison", -10, 0, false, "Goes down so smooth it's criminal.") },
-    { twig: new Item("A broken twig", 0, -.5, true, "It's broken, you're going to do less damage with this.") }
-]
+    potion: new Item("Health Potion", 10, 0, true, "Tastes like cherries!"),
+    sword: new Item("Longsword", 0, .5, true, "A shiny longsword with a sharp edge."),
+    poison: new Item("Alien Ant Poison", -10, 0, false, "Goes down so smooth it's criminal."),
+    twig: new Item("A broken twig", 0, -.5, true, "It's broken, you're going to do less damage with this.")
+}
+var randomProperty = function (object) {
+    var keys = Object.keys(object);
+    return object[keys[Math.floor(keys.length * Math.random())]];
+};
 function giveItem(item) {
-    debugger;
     if (!item) {
         // math stuff to give a random item
-        // THIS ONE IS SUPER BROKEN, FIX IT
-        items[0].Item.add();
+        randomProperty(items).add();
     }
+    else items[item].add();
+    update();
     // Gives the player a random item if !item
 }
 function drawInventory() {
     // Lists all items in player.inventory in target div
+    $("#whatever").empty();
     for (var i = 0; i < player.inventory.length; i++) {
         player.inventory[i].draw();
     }
